da5d4c20aff18e0046a6ce0066d037d5827af6be
Ryan Liew <fatal-fates@hotmail.com>
2017-11-19 23:22:39 +0800

    removed team manager and group manager column

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -29,8 +29,6 @@ class CreateUsersTable extends Migration
             $table->boolean('is_verified')->default(false);
             $table->boolean('is_default_password')->default(true);
             $table->boolean('is_admin')->default(false);
-            $table->boolean('is_team_leader')->default(false);
-            $table->boolean('is_group_manager')->default(false);
             $table->unsignedInteger('area_id')->nullable();
             $table->rememberToken();
             $table->timestamps();

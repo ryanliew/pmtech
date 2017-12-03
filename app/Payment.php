7e74b986f833e61af4c719611f40db3586a21b5e
Ryan Liew <fatal-fates@hotmail.com>
2017-12-04 01:31:55 +0800

    Added payment user relation

--- app/Payment.php
+++ app/Payment.php
@@ -8,6 +8,11 @@ class Payment extends Model
 {
 	protected $guarded = [];
 
+    public function user()
+    {
+    	return $this->belongsTo('App\User');
+    }
+    
     public function getPaymentSlipPathAttribute($payment)
     {
         return asset( $payment ? 'storage/' . $payment : "" );

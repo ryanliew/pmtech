669c7fb769baa74c90373374aec30f1f5e6a1fdc
Ryan Liew <fatal-fates@hotmail.com>
2017-12-04 17:59:12 +0800

    Added email verification

---
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Http\Request;
+
+class RegistrationConfirmationController extends Controller
+{
+    //
+    //
+    public function index()
+    {
+
+    	$user = User::where('confirmation_token', request('token'))->first();
+
+        if(! $user) {
+            return redirect(route('home'))->with('flash', 'Unknown token');
+        }
+        
+        $user->confirm();
+
+    	return redirect(route('home'))
+    			->with('flash', 'Your email is now confirmed!');
+    }	
+}

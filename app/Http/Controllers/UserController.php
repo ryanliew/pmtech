7697980fcee14d97e7129a5234b0f34cfe95f30b
Ryan Liew <fatal-fates@hotmail.com>
2017-12-20 00:12:28 +0800

    Corrected user list

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -23,7 +23,7 @@ class UserController extends Controller
 
     public function index()
     {
-    	return view('users.index', ['users' => User::latest()->with('units')->paginate(20)]);
+    	return view('users.index', ['users' => User::latest()->with('units')->get()]);
     }
 
     public function payments()

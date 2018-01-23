8f03473014009b98ba473680ccfdf8d1645941a2
Ryan Liew <fatal-fates@hotmail.com>
2018-01-23 23:53:46 +0800

    Added style version to force refresh

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
     
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css?v=1.0') }}" rel="stylesheet">
     
     <!-- Toast style -->
     <link href="{{ asset('js/vendors/jquery-toast-plugin/jquery.toast.min.css') }}" rel="stylesheet">
--- resources/views/layouts/outer-layout.blade.php
+++ resources/views/layouts/outer-layout.blade.php
@@ -11,7 +11,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
     
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css?v=1.0') }}" rel="stylesheet">
 
     <!-- Theme style -->
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">

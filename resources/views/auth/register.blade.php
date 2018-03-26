a99c48a4af8b33cbffc661253c95a49218fa3f82
Ryan Liew <fatal-fates@hotmail.com>
2018-03-26 23:28:06 +0800

    Allow user to register without contract

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -155,7 +155,7 @@
                                             @endif
                                             </div>
                                             <div class="form-group{{ $errors->has('contract_upload') ? ' has-error has-danger' : '' }} mb-30">
-                                                <label class="control-label mb-10 text-left">Management agreement upload <span class="text-danger">*</span></label>
+                                                <label class="control-label mb-10 text-left">Management agreement upload </label>
                                                 <div class="fileinput fileinput-new input-group" data-provides="fileinput">
                                                     <div class="form-control" data-trigger="fileinput"> <i class="glyphicon glyphicon-file fileinput-exists"></i> <span class="fileinput-filename"></span></div>
                                                     <span class="input-group-addon fileupload btn btn-info btn-anim btn-file"><i class="fa fa-upload"></i> <span class="fileinput-new btn-text">Select file</span> <span class="fileinput-exists btn-text">Change</span>
